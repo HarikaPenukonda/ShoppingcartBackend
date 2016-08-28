bad7515f33284a0a57d04c8db82b6f9c536867c7
HarikaPenukonda <penukondaharika@gmail.com>
2016-08-28 11:55:48 +0530

    Update UserDetails.java

--- UserDetails.java
+++ UserDetails.java
@@ -1,6 +1,8 @@
 package com.niit.shoppingcart.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,11 +14,17 @@ import org.springframework.stereotype.Component;
 public class UserDetails {
 	
 	@Id
-	private String id;
-	public String getId() {
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+    private String name;
+	private String password;
+	private String mail;
+	private String contact;
+	private String address;
+	public int getId() {
 		return id;
 	}
-	public void setId(String id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 	public String getName() {
@@ -49,12 +57,7 @@ public class UserDetails {
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	private String name;
-	private String password;
-	private String mail;
-	private String contact;
-	private String address;
-
+    
 
 
 
