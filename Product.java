65b1964ad626792f8fcc015b3a1aae330cad6ea4
HarikaPenukonda <penukondaharika@gmail.com>
2016-08-28 11:51:39 +0530

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,6 +1,9 @@
 package com.niit.shoppingcart.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import org.springframework.stereotype.Component;
@@ -10,10 +13,16 @@ import org.springframework.stereotype.Component;
 @Component
 
 public class Product {
-	public String getId() {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String description;
+	private int price;
+	public int getId() {
 		return id;
 	}
-	public void setId(String id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 	public String getName() {
@@ -28,16 +37,13 @@ public class Product {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public String getPrice() {
+	public int getPrice() {
 		return price;
 	}
-	public void setPrice(String price) {
+	public void setPrice(int price) {
 		this.price = price;
 	}
-	private String id;
-	private String name;
-	private String description;
-	private String price;
+	
 
 
 }
